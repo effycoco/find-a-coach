e582166991e6db692501ff0181233387b96fa504
effycoco <734157786@qq.com>
2022-08-02 16:36:08 +0800

    after gh-page deploy

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+
+
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录下
+cd dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:effycoco/find-a-coach.git master:gh-pages
+# 关联并上传至gh-pages分支
+
+cd -
